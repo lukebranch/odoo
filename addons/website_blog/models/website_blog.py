be3b866719ed28d16ba5ca36c65c383ac029240a
Mahendra barad <mba@openerp.com>
2014-09-15 18:12:22 +0530

    [MIG]migrate to new api
    [IMP]remove unneccessary stuff
    
    [IMP]remove dead code
    
    [IMP]improve the ranking function and some code
    
    [IMP]rename the moethod _postproces_content ro _postprocess_content and improve the mail message disucssion method
    
    [IMP]improve the code
    
    [IMP]make related field store because it's not working on website qweb error
    
    [IMP]conver some code after rebase
    
    [IMP]improve the code after rebase
    
    [IMP]rename the _obj and openepr tag

--- addons/website_blog/models/website_blog.py
+++ addons/website_blog/models/website_blog.py
@@ -4,24 +4,21 @@ from datetime import datetime
 import lxml
 import random
 
-from openerp import tools
-from openerp import SUPERUSER_ID
+from openerp import api, fields, models, tools, _
 from openerp.addons.website.models.website import slug
-from openerp.osv import osv, fields
-from openerp.tools.translate import _
 
 
-class Blog(osv.Model):
+class Blog(models.Model):
     _name = 'blog.blog'
     _description = 'Blogs'
     _inherit = ['mail.thread', 'website.seo.metadata']
     _order = 'name'
-    _columns = {
-        'name': fields.char('Blog Name', required=True, translate=True),
-        'subtitle': fields.char('Blog Subtitle', translate=True),
-    }
 
-    def all_tags(self, cr, uid, ids, min_limit=1, context=None):
+    name = fields.Char('Blog Name', required=True, translate=True)
+    subtitle = fields.Char('Blog Subtitle', translate=True)
+
+    @api.multi
+    def all_tags(self, min_limit=1):
         req = """
             SELECT
                 p.blog_id, count(*), r.blog_tag_id
@@ -36,111 +33,80 @@ class Blog(osv.Model):
             ORDER BY
                 count(*) DESC
         """
-        cr.execute(req, [tuple(ids)])
-        tag_by_blog = {i: [] for i in ids}
-        for blog_id, freq, tag_id in cr.fetchall():
+        self.env.cr.execute(req, [tuple(self.ids)])
+        tag_by_blog = {i: [] for i in self.ids}
+        for blog_id, freq, tag_id in self.env.cr.fetchall():
             if freq >= min_limit:
                 tag_by_blog[blog_id].append(tag_id)
 
-        tag_obj = self.pool['blog.tag']
         for blog_id in tag_by_blog:
-            tag_by_blog[blog_id] = tag_obj.browse(cr, uid, tag_by_blog[blog_id], context=context)
+            tag_by_blog[blog_id] = self.env['blog.tag'].browse(tag_by_blog[blog_id])
         return tag_by_blog
 
 
-class BlogTag(osv.Model):
+class BlogTag(models.Model):
     _name = 'blog.tag'
     _description = 'Blog Tag'
     _inherit = ['website.seo.metadata']
     _order = 'name'
-    _columns = {
-        'name': fields.char('Name', required=True),
-        'post_ids': fields.many2many(
-            'blog.post', string='Posts',
-        ),
-    }
 
+    name = fields.Char(string='Name', required=True)
+    post_ids = fields.Many2many('blog.post', string="Posts")
 
-class BlogPost(osv.Model):
+class BlogPost(models.Model):
     _name = "blog.post"
     _description = "Blog Post"
     _inherit = ['mail.thread', 'website.seo.metadata', 'website.published.mixin']
     _order = 'id DESC'
     _mail_post_access = 'read'
 
-    def _website_url(self, cr, uid, ids, field_name, arg, context=None):
-        res = super(BlogPost, self)._website_url(cr, uid, ids, field_name, arg, context=context)
-        for blog_post in self.browse(cr, uid, ids, context=context):
-            res[blog_post.id] = "/blog/%s/post/%s" % (slug(blog_post.blog_id), slug(blog_post))
+    @api.multi
+    def _website_url(self, field_name, arg):
+        res = super(BlogPost, self)._website_url(field_name, arg)
+        res.update({(blog_post.id, '/blog/%s/post/%s' % (slug(blog_post.blog_id), slug(blog_post))) for blog_post in self})
         return res
 
-    def _compute_ranking(self, cr, uid, ids, name, arg, context=None):
-        res = {}
-        for blog_post in self.browse(cr, uid, ids, context=context):
-            age = datetime.now() - datetime.strptime(blog_post.create_date, tools.DEFAULT_SERVER_DATETIME_FORMAT)
-            res[blog_post.id] = blog_post.visits * (0.5+random.random()) / max(3, age.days)
-        return res
-
-    def _default_content(self, cr, uid, context=None):
+    def _default_content(self):
         return '''  <div class="container">
                         <section class="mt16 mb16">
                             <p class="o_default_snippet_text">''' + _("Start writing here...") + '''</p>
                         </section>
                     </div> '''
 
-    _columns = {
-        'name': fields.char('Title', required=True, translate=True),
-        'subtitle': fields.char('Sub Title', translate=True),
-        'author_id': fields.many2one('res.partner', 'Author'),
-        'cover_properties': fields.text('Cover Properties'),
-        'blog_id': fields.many2one(
-            'blog.blog', 'Blog',
-            required=True, ondelete='cascade',
-        ),
-        'tag_ids': fields.many2many(
-            'blog.tag', string='Tags',
-        ),
-        'content': fields.html('Content', translate=True, sanitize=False),
-        'website_message_ids': fields.one2many(
-            'mail.message', 'res_id',
-            domain=lambda self: [
-                '&', '&', ('model', '=', self._name), ('type', '=', 'comment'), ('path', '=', False)
-            ],
-            string='Website Messages',
-            help="Website communication history",
-        ),
-        # creation / update stuff
-        'create_date': fields.datetime(
-            'Created on',
-            select=True, readonly=True,
-        ),
-        'create_uid': fields.many2one(
-            'res.users', 'Author',
-            select=True, readonly=True,
-        ),
-        'write_date': fields.datetime(
-            'Last Modified on',
-            select=True, readonly=True,
-        ),
-        'write_uid': fields.many2one(
-            'res.users', 'Last Contributor',
-            select=True, readonly=True,
-        ),
-        'author_avatar': fields.related(
-            'author_id', 'image_small',
-            string="Avatar", type="binary"),
-        'visits': fields.integer('No of Views'),
-        'ranking': fields.function(_compute_ranking, string='Ranking', type='float'),
-    }
+    @api.multi
+    @api.depends('visits')
+    def _compute_ranking(self):
+        for blog_post in self:
+            if blog_post.visits:
+                age = datetime.now() - datetime.strptime(blog_post.create_date, tools.DEFAULT_SERVER_DATETIME_FORMAT)
+                blog_post.ranking = blog_post.visits * (0.5+random.random()) / max(3, age.days)
 
-    _defaults = {
-        'name': '',
-        'content': _default_content,
-        'cover_properties': '{"background-image": "none", "background-color": "oe_none", "opacity": "0.6", "resize_class": ""}',
-        'author_id': lambda self, cr, uid, ctx=None: self.pool['res.users'].browse(cr, uid, uid, context=ctx).partner_id.id,
-    }
+    name = fields.Char(string='Title', required=True, translate=True, default='')
+    subtitle = fields.Char(string='Sub Title', translate=True, default=_('Subtitle'))
+    author_id = fields.Many2one('res.partner', string='Author', default=lambda self:self.env.user.partner_id)
+    cover_properties = fields.Text(string='Cover Properties', default='{"background-image": "none", "background-color": "oe_none", "opacity": "0.6", "resize_class": ""}')
+    blog_id = fields.Many2one(
+        'blog.blog', string='Blog',
+        required=True, ondelete='cascade'
+    )
+    tag_ids = fields.Many2many(
+        'blog.tag', string='Tags'
+    )
+    content = fields.Html(string='Content', translate=True, sanitize=False, default=_default_content)
+    website_message_ids = fields.One2many(
+        'mail.message', 'res_id',
+        domain=lambda self: [
+            '&', '&', ('model', '=', self._name), ('type', '=', 'comment'), ('path', '=', False)
+        ],
+        string='Website Messages',
+        help="Website communication history"
+    )
+    author_avatar = fields.Binary(related="author_id.image_small", string="Avatar")
+    visits = fields.Integer(string='No of Views')
+    ranking = fields.Float(compute='_compute_ranking', string='Ranking')
 
-    def html_tag_nodes(self, html, attribute=None, tags=None, context=None):
+    @api.model
+    def html_tag_nodes(self, html, attribute=None, tags=None):
         """ Processing of html content to tag paragraphs and set them an unique
         ID.
         :return result: (html, mappin), where html is the updated html with ID
@@ -188,29 +154,32 @@ class BlogPost(osv.Model):
             html = html[5:-6]
         return html, mapping
 
-    def _postproces_content(self, cr, uid, id, content=None, context=None):
+    @api.model
+    def _postprocess_content(self, id, content=None):
+        #(_postproces_content = _postprocess_content)
         if content is None:
-            content = self.browse(cr, uid, id, context=context).content
+            content = self.browse(id).content
         if content is False:
             return content
-
-        content, mapping = self.html_tag_nodes(content, attribute='data-chatter-id', tags=['p'], context=context)
+        content, mapping = self.html_tag_nodes(content, attribute='data-chatter-id', tags=['p'])
         if id:  # not creating
             existing = [x[0] for x in mapping if x[0]]
-            msg_ids = self.pool['mail.message'].search(cr, SUPERUSER_ID, [
+            msg_ids = self.env['mail.message'].sudo().search([
                 ('res_id', '=', id),
                 ('model', '=', self._name),
                 ('path', 'not in', existing),
                 ('path', '!=', False)
-            ], context=context)
-            self.pool['mail.message'].unlink(cr, SUPERUSER_ID, msg_ids, context=context)
+            ])
+            if msg_ids:
+                msg_ids.unlink()
 
         return content
 
-    def _check_for_publication(self, cr, uid, ids, vals, context=None):
+    @api.multi
+    def _check_for_publication(self, vals):
         if vals.get('website_published'):
-            base_url = self.pool['ir.config_parameter'].get_param(cr, uid, 'web.base.url')
-            for post in self.browse(cr, uid, ids, context=context):
+            base_url = self.env['ir.config_parameter'].get_param('web.base.url')
+            for post in self:
                 post.blog_id.message_post(
                     body='<p>%(post_publication)s <a href="%(base_url)s/blog/%(blog_slug)s/post/%(post_slug)s">%(post_link)s</a></p>' % {
                         'post_publication': _('A new post %s has been published on the %s blog.') % (post.name, post.blog_id.name),
@@ -219,49 +188,45 @@ class BlogPost(osv.Model):
                         'blog_slug': slug(post.blog_id),
                         'post_slug': slug(post),
                     },
-                    subtype='website_blog.mt_blog_blog_published',
-                    context=context)
+                    subtype='website_blog.mt_blog_blog_published')
             return True
         return False
 
-    def create(self, cr, uid, vals, context=None):
-        if context is None:
-            context = {}
+    @api.model
+    def create(self, vals):
         if 'content' in vals:
-            vals['content'] = self._postproces_content(cr, uid, None, vals['content'], context=context)
-        create_context = dict(context, mail_create_nolog=True)
-        post_id = super(BlogPost, self).create(cr, uid, vals, context=create_context)
-        self._check_for_publication(cr, uid, [post_id], vals, context=context)
-        return post_id
+            vals['content'] = self._postprocess_content(None, vals['content'])
+        post = super(BlogPost, self.with_context(mail_create_nolog=True)).create(vals)
+        post._check_for_publication(vals)
+        return post
 
-    def write(self, cr, uid, ids, vals, context=None):
+    @api.multi
+    def write(self, vals):
         if 'content' in vals:
-            vals['content'] = self._postproces_content(cr, uid, ids[0], vals['content'], context=context)
-        result = super(BlogPost, self).write(cr, uid, ids, vals, context)
-        self._check_for_publication(cr, uid, ids, vals, context=context)
+            vals['content'] = self._postprocess_content(self.id, vals['content'])
+        result = super(BlogPost, self).write(vals)
+        self._check_for_publication(vals)
         return result
 
 
-class Website(osv.Model):
+class Website(models.Model):
     _inherit = "website"
 
-    def page_search_dependencies(self, cr, uid, view_id, context=None):
-        dep = super(Website, self).page_search_dependencies(cr, uid, view_id, context=context)
-
-        post_obj = self.pool.get('blog.post')
-
-        view = self.pool.get('ir.ui.view').browse(cr, uid, view_id, context=context)
+    @api.model
+    def page_search_dependencies(self, view_id):
+        dep = super(Website, self).page_search_dependencies(view_id)
+        view = self.env['ir.ui.view'].browse(view_id)
         name = view.key.replace("website.", "")
         fullname = "website.%s" % name
 
         dom = [
             '|', ('content', 'ilike', '/page/%s' % name), ('content', 'ilike', '/page/%s' % fullname)
         ]
-        posts = post_obj.search(cr, uid, dom, context=context)
+        posts = self.env['blog.post'].search(dom)
         if posts:
             page_key = _('Blog Post')
             dep[page_key] = []
-        for p in post_obj.browse(cr, uid, posts, context=context):
+        for p in posts:
             dep[page_key].append({
                 'text': _('Blog Post <b>%s</b> seems to have a link to this page !' % p.name),
                 'link': p.website_url
