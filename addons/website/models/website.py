14484d5fee17687b54dccf3c737b689f78e2ca2f
David Monjoie <dmo@odoo.com>
2015-01-19 17:32:05 +0100

    [IMP] [REF] website: introducing a mixin class to handle website_published and website_url fields. This will allow to remove custom code in every website addons that redefine those two fields and the URL getter method.
    The published button in backend has also been cleaned. It is now a link button
    that redirects to the website_url. It does not change the publish status
    anymore. Indeed it makes sense publish and unpublish website documents
    directly in the website, while checking that everything is correct, instead
    of doing it in the backend.
    
    Next commit will update the Odoo addons to use this mixin and the new button
    in the form view.

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -853,3 +853,25 @@ class website_seo_metadata(osv.Model):
         'website_meta_description': fields.text("Website meta description", translate=True),
         'website_meta_keywords': fields.char("Website meta keywords", translate=True),
     }
+
+
+class website_published_mixin(osv.AbstractModel):
+    _name = "website.published.mixin"
+
+    _website_url_proxy = lambda self, *a, **kw: self._website_url(*a, **kw)
+
+    _columns = {
+        'website_published': fields.boolean('Visible in Website', copy=False),
+        'website_url': fields.function(_website_url_proxy, type='char', string='Website URL',
+                                       help='The full URL to access the document through the website.'),
+    }
+
+    def _website_url(self, cr, uid, ids, field_name, arg, context=None):
+        return dict.fromkeys(ids, '#')
+
+    def open_website_url(self, cr, uid, id, context=None):
+        return {
+            'type': 'ir.actions.act_url',
+            'url': self.browse(cr, uid, id).website_url,
+            'target': 'self',
+        }
