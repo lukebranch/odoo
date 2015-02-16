c7eb71df2d7dcb5485f9fa2994af31f5ddce1284
Josse Colpaert <jco@odoo.com>
2015-02-16 16:29:56 +0100

    [IMP] Change name of tests folder in account, so account tests get only executed in l10n

--- addons/l10n_be/tests/test_do.py
+++ addons/l10n_be/tests/test_do.py
@@ -1,6 +1,6 @@
 from openerp.tests.common import TransactionCase
-from openerp.addons.account.tests.test_bank_statement_reconciliation import TestBankStatementReconciliation
-from openerp.addons.account.tests.test_tax import TestTax
+from openerp.addons.account.tests_l10n.test_bank_statement_reconciliation import TestBankStatementReconciliation
+from openerp.addons.account.tests_l10n.test_tax import TestTax
 
 class TestDo(TestBankStatementReconciliation):
     pass
