1b949dbf00592c93a55822dae8d7706bdfcb3495
Martin Geubelle <mge@odoo.com>
2015-02-20 13:44:33 +0100

    [ADD] New module

---
+++ addons/account_contract_dashboard/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'Account Contract Dashboard',
+    'version': '1.0',
+    'depends': ['account_analytic_analysis', 'account_asset', 'website'],
+    'author': 'Odoo S.A.',
+    'description': """
+    Account Contract Dashboard
+    """,
+    'website': 'https://www.odoo.com/page/accounting',
+    'category': 'Accounting & Finance',
+    'demo': [],
+    'data': [
+        # 'security/account_contract_dashboard.xml',
+        # 'security/ir.model.access.csv',)
+        'report/account_contract_dashboard_report_view.xml',
+        'templates/dashboard.xml',
+        'templates/assets.xml',
+    ],
+    # 'qweb': [
+    #     "static/src/xml/account_asset_template.xml",
+    # ],
+    'installable': True,
+    'application': False,
+}
---
+++ addons/account_contract_dashboard/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- encoding: utf-8 -*-
+
+import account_contract_dashboard
