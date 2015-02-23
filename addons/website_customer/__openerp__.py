c0b7ba31e51a0305913630b48f2f7fec5a3a1db1
Ravi Gohil <rgo@odoo.com>
2015-02-23 11:41:40 +0530

    [website_customer] Migrated to new api.

--- addons/website_customer/__openerp__.py
+++ addons/website_customer/__openerp__.py
@@ -1,24 +1,4 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2013-Today OpenERP S.A. (<http://www.openerp.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 {
     'name': 'Customer References',
     'category': 'Website',
@@ -26,10 +6,10 @@
     'summary': 'Publish Your Customer References',
     'version': '1.0',
     'description': """
-OpenERP Customer References
+Odoo Customer References
 ===========================
 """,
-    'author': 'OpenERP SA',
+    'author': 'Odoo S.A.',
     'depends': [
         'crm_partner_assign',
         'website_partner',
--- addons/website_customer/models/res_partner.py
+++ addons/website_customer/models/res_partner.py
@@ -1,33 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import osv, fields
+from openerp import fields, models
 
 
-class res_partner(osv.Model):
+class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    _columns = {
-        'tag_ids': fields.many2many('res.partner.tag', id1='partner_id', id2='tag_id', string='Tags'),
-    }
+    tag_ids = fields.Many2many('res.partner.tag', column1='partner_id', column2='tag_id', string='Tags')
 
 
-class res_partner_tags(osv.Model):
+class ResPartnerTags(models.Model):
     _description = 'Partner Tags - These tags can be used on website to find customers by sector, or ... '
     _name = 'res.partner.tag'
     _inherit = 'website.published.mixin'
 
-    def get_selection_class(self, cr, uid, context=None):
+    name = fields.Char(string='Category Name', required=True, translate=True)
+    partner_ids = fields.Many2many('res.partner',
+        'res_partner_res_partner_tag_rel', 'tag_id', 'partner_id',
+        string='Partners')
+    classname = fields.Selection('get_selection_class', string='Class',
+        help="Bootstrap class to customize the color of the tag", required=True,
+        default='default')
+    active = fields.Boolean(default=True)
+    website_published = fields.Boolean(default=True)
+
+    def get_selection_class(self):
         classname = ['default', 'primary', 'success', 'warning', 'danger']
         return [(x, str.title(x)) for x in classname]
-
-    _columns = {
-        'name': fields.char('Category Name', required=True, translate=True),
-        'partner_ids': fields.many2many('res.partner', 'res_partner_res_partner_tag_rel', id1='tag_id', id2='partner_id', string='Partners'),
-        'classname': fields.selection(get_selection_class, 'Class', help="Bootstrap class to customize the color of the tag", required=True),
-        'active': fields.boolean('Active'),
-    }
-    _defaults = {
-        'active': True,
-        'website_published': True,
-        'classname': 'default',
-    }
