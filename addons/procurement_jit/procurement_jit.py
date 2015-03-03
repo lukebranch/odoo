2024ad9bb9ef91eaf09eb8cd7d501dab96af6e66
Josse Colpaert <jco@odoo.com>
2015-03-03 14:46:17 +0100

    [IMP] Make sure procurements are run in the original order

--- addons/procurement_jit/procurement_jit.py
+++ addons/procurement_jit/procurement_jit.py
@@ -38,7 +38,7 @@ class procurement_order(osv.osv):
         context['procurement_autorun_defer'] = True
         res = super(procurement_order, self).run(cr, uid, ids, context=context)
 
-        procurement_ids = self.search(cr, uid, [('move_dest_id.procurement_id', 'in', ids)], context=context)
+        procurement_ids = self.search(cr, uid, [('move_dest_id.procurement_id', 'in', ids)], order='id', context=context)
 
         if procurement_ids:
             return self.run(cr, uid, procurement_ids, context=context)
