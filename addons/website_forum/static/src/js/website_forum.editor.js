1a8b408be5055b16f9e62b7eb22605e45e937d27
Géry Debongnie <ged@odoo.com>
2015-03-10 08:50:15 +0100

    [REF] website_forum: update to the new module system

--- addons/website_forum/static/src/js/website_forum.editor.js
+++ addons/website_forum/static/src/js/website_forum.editor.js
@@ -1,20 +1,24 @@
-(function() {
-    "use strict";
+odoo.define('website_forum.editor', ['web.core', 'website.contentMenu', 'website.website'], function (require) {
+"use strict";
 
-    var website = openerp.website;
-    var _t = openerp._t;
+var core = require('web.core');
+var contentMenu = require('website.contentMenu');
+var website = require('website.website');
 
-    website.EditorBarContent.include({
-        new_forum: function() {
-            website.prompt({
-                id: "editor_new_forum",
-                window_title: _t("New Forum"),
-                input: "Forum Name",
-            }).then(function (forum_name) {
-                website.form('/forum/new', 'POST', {
-                    forum_name: forum_name
-                });
+var _t = core._t;
+
+contentMenu.EditorBarContent.include({
+    new_forum: function() {
+        website.prompt({
+            id: "editor_new_forum",
+            window_title: _t("New Forum"),
+            input: "Forum Name",
+        }).then(function (forum_name) {
+            website.form('/forum/new', 'POST', {
+                forum_name: forum_name
             });
-        },
-    });
-})();
+        });
+    },
+});
+
+});
