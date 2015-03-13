d73eb25c628c04315143baf64af742e8a4bc2e96
mge-odoo <mge@odoo.com>
2015-03-13 12:17:37 +0100

    [ADD] Own layout to simplify JS

--- addons/account_contract_dashboard/static/src/js/account_contract_dashboard_backend.js
+++ addons/account_contract_dashboard/static/src/js/account_contract_dashboard_backend.js
@@ -9,35 +9,8 @@ openerp.account_contract_dashboard = function (instance) {
             var self = this;
             this._super();
             this.$el.attr({'style': 'height: 100%; width: 100%; border: 0;', 'src': '/account_contract_dashboard'});
-            this.$el.load(function() {
-                self.$el.contents().find('#oe_main_menu_navbar').hide();
-                self.$el.contents().find('header').hide();
-                self.$el.contents().find('footer').hide();
-                self.$el.contents().find('#wrapwrap').addClass('no-padding');
-            });
         }
     });
 
     instance.web.client_actions.add("account_contract_dashboard", "instance.web.account_contract_dashboard");
-
 };
-
-// CORRECT VERSION
-
-// (function() {
-//     openerp.account_contract = {};
-
-//     openerp.account_contract.account_contract_dashboard = openerp.web.FrontendInBackend.extend({
-//         get_url: function() {
-//             return '/account_contract_dashboard';
-//         },
-//         posprocess_iframe: function() {
-//             var myIframe = $('iframe').contents();
-//             myIframe.find('#oe_main_menu_navbar').hide();
-//             myIframe.find('header').hide();
-//             myIframe.find('footer').hide();
-//         },        
-//     });
-
-//     openerp.web.client_actions.add("account_contract_dashboard", "openerp.account_contract.account_contract_dashboard");
-// })();
