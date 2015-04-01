c9f142bc44dee61071c15aa260f9c5a77a5f5e56
Josse Colpaert <jco@odoo.com>
2015-04-01 18:01:33 +0200

    [IMP] stock: do same with mto whether from orderpoint or directly from move

--- addons/procurement/procurement.py
+++ addons/procurement/procurement.py
@@ -329,24 +329,23 @@ class procurement_order(osv.osv):
                 raise osv.except_osv(_('Data Insufficient!'),
                     _('Please check the quantity in procurement order(s) for the product "%s", it should not be 0 or less!' % procurement.product_id.name))
             if procurement.product_id.type in ('product', 'consu'):
-                if not procurement.move_id:
-                    source = procurement.location_id.id
-                    if procurement.procure_method == 'make_to_order':
-                        source = procurement.product_id.property_stock_procurement.id
-                    id = move_obj.create(cr, uid, {
-                        'name': procurement.name,
-                        'location_id': source,
-                        'location_dest_id': procurement.location_id.id,
-                        'product_id': procurement.product_id.id,
-                        'product_qty': procurement.product_qty,
-                        'product_uom': procurement.product_uom.id,
-                        'date_expected': procurement.date_planned,
-                        'state': 'draft',
-                        'company_id': procurement.company_id.id,
-                        'auto_validate': True,
-                    })
-                    move_obj.action_confirm(cr, uid, [id], context=context)
-                    self.write(cr, uid, [procurement.id], {'move_id': id, 'close_move': 1})
+                source = procurement.location_id.id
+                if procurement.procure_method == 'make_to_order':
+                    source = procurement.product_id.property_stock_procurement.id
+                id = move_obj.create(cr, uid, {
+                    'name': procurement.name,
+                    'location_id': source,
+                    'location_dest_id': procurement.location_id.id,
+                    'product_id': procurement.product_id.id,
+                    'product_qty': procurement.product_qty,
+                    'product_uom': procurement.product_uom.id,
+                    'date_expected': procurement.date_planned,
+                    'state': 'draft',
+                    'company_id': procurement.company_id.id,
+                    'auto_validate': True,
+                })
+                move_obj.action_confirm(cr, uid, [id], context=context)
+                self.write(cr, uid, [procurement.id], {'move_id': id, 'close_move': 1})
         self.write(cr, uid, ids, {'state': 'confirmed', 'message': ''})
         return True
 
