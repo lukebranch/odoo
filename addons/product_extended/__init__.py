e2e5106133f5a770531984bc1007fd05b12289ef
Gaurav Panchal <gan@openerp.com>
2015-04-03 11:50:53 +0530

    [IMP] Move py file in models directory and xml file in views directory

--- addons/product_extended/__init__.py
+++ addons/product_extended/__init__.py
@@ -17,5 +17,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
 #
 ##############################################################################
-import product_extended
+import models
 import wizard
