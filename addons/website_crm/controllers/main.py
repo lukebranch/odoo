Odew/odoo-1

[website_crm] Migrated to new api.

addons/website_crm/controllers/main.py
@@ -4,12 +4,11 @@ import base64
 import werkzeug
 import werkzeug.urls
 
-from openerp import http, SUPERUSER_ID
+from openerp import _, http, SUPERUSER_ID
 from openerp.http import request
-from openerp.tools.translate import _
 
 
-class contactus(http.Controller):
+class ContactUs(http.Controller):
 
     def generate_google_map_url(self, street, city, city_zip, country_name):
         url = "http://maps.googleapis.com/maps/api/staticmap?center=%s&sensor=false&zoom=8&size=298x298" % werkzeug.url_quote_plus(
@@ -28,8 +27,11 @@ class contactus(http.Controller):
 
     def create_lead(self, request, values, kwargs):
         """ Allow to be overrided """
-        cr, context = request.cr, request.context
-        return request.registry['crm.lead'].create(cr, SUPERUSER_ID, values, context=dict(context, mail_create_nosubscribe=True))
+        """
+        Note: While refactoring, check the usage of 'request' and 'kwargs' parameters
+              and if they are needed here.
+        """
+        return request.env['crm.lead'].with_context(mail_create_nosubscribe=True).sudo().create(values).id
 
     def preRenderThanks(self, values, kwargs):
         """ Allow to be overrided """
@@ -60,13 +62,13 @@ class contactus(http.Controller):
         post_description = []  # Info to add after the message
         values = {}
 
-        values['medium_id'] = request.registry['ir.model.data'].xmlid_to_res_id(request.cr, SUPERUSER_ID, 'crm.crm_medium_website')
-        values['team_id'] = request.registry['ir.model.data'].xmlid_to_res_id(request.cr, SUPERUSER_ID, 'website.salesteam_website_sales')
+        values['medium_id']  = request.env(user=SUPERUSER_ID).ref('utm.utm_medium_website').id
+        values['team_id'] = request.env(user=SUPERUSER_ID).ref('website.salesteam_website_sales').id
 
         for field_name, field_value in kwargs.items():
             if hasattr(field_value, 'filename'):
                 post_file.append(field_value)
-            elif field_name in request.registry['crm.lead']._fields and field_name not in _BLACKLIST:
+            elif field_name in request.env['crm.lead']._fields and field_name not in _BLACKLIST:
                 values[field_name] = field_value
             elif field_name not in _TECHNICAL:  # allow to add some free fields or blacklisted field like ID
                 post_description.append("%s: %s" % (field_name, field_value))
@@ -105,6 +107,6 @@ class contactus(http.Controller):
                     'datas': base64.encodestring(field_value.read()),
                     'datas_fname': field_value.filename,
                 }
-                request.registry['ir.attachment'].create(request.cr, SUPERUSER_ID, attachment_value, context=request.context)
+                request.env['ir.attachment'].sudo().create(attachment_value)
 
         return self.get_contactus_response(values, kwargs)

lukebranch/odoo

addons/website_crm/controllers/main.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
+
 import base64
 
 import werkzeug
 import werkzeug.urls
 
-from openerp import http, SUPERUSER_ID
+from openerp import http, SUPERUSER_ID, _
 from openerp.http import request
-from openerp.tools.translate import _
 
 
-class contactus(http.Controller):
+class ContactUs(http.Controller):
 
     def generate_google_map_url(self, street, city, city_zip, country_name):
         url = "http://maps.googleapis.com/maps/api/staticmap?center=%s&sensor=false&zoom=8&size=298x298" % werkzeug.url_quote_plus(
@@ -28,8 +28,11 @@ class contactus(http.Controller):
 
     def create_lead(self, request, values, kwargs):
         """ Allow to be overrided """
-        cr, context = request.cr, request.context
-        return request.registry['crm.lead'].create(cr, SUPERUSER_ID, values, context=dict(context, mail_create_nosubscribe=True))
+        """
+        Note: While refactoring, check the usage of 'request' and 'kwargs' parameters
+              and if they are needed here.
+        """
+        return request.env['crm.lead'].with_context(mail_create_nosubscribe=True).sudo().create(values).id
 
     def preRenderThanks(self, values, kwargs):
         """ Allow to be overrided """
@@ -60,13 +63,13 @@ class contactus(http.Controller):
         post_description = []  # Info to add after the message
         values = {}
 
-        values['medium_id'] = request.registry['ir.model.data'].xmlid_to_res_id(request.cr, SUPERUSER_ID, 'crm.crm_medium_website')
-        values['team_id'] = request.registry['ir.model.data'].xmlid_to_res_id(request.cr, SUPERUSER_ID, 'website.salesteam_website_sales')
+        values['medium_id'] = request.env(user=SUPERUSER_ID).ref('utm.utm_medium_website').id
+        values['team_id'] = request.env(user=SUPERUSER_ID).ref('website.salesteam_website_sales').id
 
         for field_name, field_value in kwargs.items():
             if hasattr(field_value, 'filename'):
                 post_file.append(field_value)
-            elif field_name in request.registry['crm.lead']._fields and field_name not in _BLACKLIST:
+            elif field_name in request.env['crm.lead']._fields and field_name not in _BLACKLIST:
                 values[field_name] = field_value
             elif field_name not in _TECHNICAL:  # allow to add some free fields or blacklisted field like ID
                 post_description.append("%s: %s" % (field_name, field_value))
@@ -105,6 +108,6 @@ class contactus(http.Controller):
                     'datas': base64.encodestring(field_value.read()),
                     'datas_fname': field_value.filename,
                 }
-                request.registry['ir.attachment'].create(request.cr, SUPERUSER_ID, attachment_value, context=request.context)
+                request.env['ir.attachment'].sudo().create(attachment_value)
 
         return self.get_contactus_response(values, kwargs)