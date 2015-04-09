Odew/odoo-1

[website_crm_partner_assign] Migrated to new api.

addons/website_crm_partner_assign/controllers/main.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 import werkzeug
-from openerp import SUPERUSER_ID
-from openerp.addons.web import http
-from openerp.addons.web.http import request
+from openerp import _, http
+from openerp.http import request
 from openerp.addons.website.models.website import slug, unslug
 from openerp.addons.website_partner.controllers.main import WebsitePartnerPage
-from openerp.tools.translate import _
 
 
 class WebsiteCrmPartnerAssign(WebsitePartnerPage):
@@ -26,12 +24,12 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
     ], type='http', auth="public", website=True)
     def partners(self, country=None, grade=None, page=0, **post):
         country_all = post.pop('country_all', False)
-        partner_obj = request.registry['res.partner']
-        country_obj = request.registry['res.country']
+        Partner_sudo = request.env['res.partner'].sudo()
         search = post.get('search', '')
+        grade_count, country_count = 0, 0
 
         base_partner_domain = [('is_company', '=', True), ('grade_id', '!=', False), ('website_published', '=', True)]
-        if request.registry['res.users'].has_group(request.cr, request.uid, 'base.group_website_publisher'):
+        if request.env['res.users'].has_group('base.group_website_publisher'):
             base_partner_domain += [('grade_id.website_published', '=', True)]
         if search:
             base_partner_domain += ['|', ('name', 'ilike', search), ('website_description', 'ilike', search)]
@@ -41,22 +39,16 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
         if not country and not country_all:
             country_code = request.session['geoip'].get('country_code')
             if country_code:
-                country_ids = country_obj.search(request.cr, request.uid, [('code', '=', country_code)], context=request.context)
-                if country_ids:
-                    country = country_obj.browse(request.cr, request.uid, country_ids[0], context=request.context)
+                country = request.env['res.country'].search([('code', '=', country_code)], limit=1)
         if country:
             grade_domain += [('country_id', '=', country.id)]
-        grades = partner_obj.read_group(
-            request.cr, SUPERUSER_ID, grade_domain, ["id", "grade_id"],
-            groupby="grade_id", orderby="grade_id DESC", context=request.context)
-        grades_partners = partner_obj.search(
-            request.cr, SUPERUSER_ID, grade_domain,
-            context=request.context, count=True)
+        grades = Partner_sudo.read_group(grade_domain, ["id", "grade_id"], groupby="grade_id", orderby="grade_id DESC")
         # flag active grade
         for grade_dict in grades:
             grade_dict['active'] = grade and grade_dict['grade_id'][0] == grade.id
+            grade_count += grade_dict['grade_id_count']
         grades.insert(0, {
-            'grade_id_count': grades_partners,
+            'grade_id_count': grade_count,
             'grade_id': (0, _("All Categories")),
             'active': bool(grade is None),
         })
@@ -65,17 +57,13 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
         country_domain = list(base_partner_domain)
         if grade:
             country_domain += [('grade_id', '=', grade.id)]
-        countries = partner_obj.read_group(
-            request.cr, SUPERUSER_ID, country_domain, ["id", "country_id"],
-            groupby="country_id", orderby="country_id", context=request.context)
-        countries_partners = partner_obj.search(
-            request.cr, SUPERUSER_ID, country_domain,
-            context=request.context, count=True)
+        countries = Partner_sudo.read_group(country_domain, ["id", "country_id"], groupby="country_id", orderby="country_id")
         # flag active country
         for country_dict in countries:
             country_dict['active'] = country and country_dict['country_id'] and country_dict['country_id'][0] == country.id
+            country_count += country_dict['country_id_count']
         countries.insert(0, {
-            'country_id_count': countries_partners,
+            'country_id_count': country_count,
             'country_id': (0, _("All Countries")),
             'active': bool(country is None),
         })
@@ -101,19 +89,13 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
         if country_all:
             url_args['country_all'] = True
 
-        partner_count = partner_obj.search_count(
-            request.cr, SUPERUSER_ID, base_partner_domain,
-            context=request.context)
+        partner_count = Partner_sudo.search_count(base_partner_domain)
         pager = request.website.pager(
             url=url, total=partner_count, page=page, step=self._references_per_page, scope=7,
             url_args=url_args)
 
         # search partners matching current search parameters
-        partner_ids = partner_obj.search(
-            request.cr, SUPERUSER_ID, base_partner_domain,
-            order="grade_id DESC",
-            context=request.context)  # todo in trunk: order="grade_id DESC, implemented_count DESC", offset=pager['offset'], limit=self._references_per_page
-        partners = partner_obj.browse(request.cr, SUPERUSER_ID, partner_ids, request.context)
+        partners = Partner_sudo.search(base_partner_domain, order="grade_id DESC")  # todo in trunk: order="grade_id DESC, implemented_count DESC", offset=pager['offset'], limit=self._references_per_page
         # remove me in trunk
         partners = sorted(partners, key=lambda x: (x.grade_id.sequence if x.grade_id else 0, len([i for i in x.implemented_partner_ids if i.website_published])), reverse=True)
         partners = partners[pager['offset']:pager['offset'] + self._references_per_page]
@@ -137,26 +119,15 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
     @http.route(['/partners/<partner_id>'], type='http', auth="public", website=True)
     def partners_detail(self, partner_id, **post):
         _, partner_id = unslug(partner_id)
-        current_grade, current_country = None, None
         grade_id = post.get('grade_id')
         country_id = post.get('country_id')
-        if grade_id:
-            grade_ids = request.registry['res.partner.grade'].exists(request.cr, request.uid, int(grade_id), context=request.context)
-            if grade_ids:
-                current_grade = request.registry['res.partner.grade'].browse(request.cr, request.uid, grade_ids[0], context=request.context)
-        if country_id:
-            country_ids = request.registry['res.country'].exists(request.cr, request.uid, int(country_id), context=request.context)
-            if country_ids:
-                current_country = request.registry['res.country'].browse(request.cr, request.uid, country_ids[0], context=request.context)
-        if partner_id:
-            partner = request.registry['res.partner'].browse(request.cr, SUPERUSER_ID, partner_id, context=request.context)
-            is_website_publisher = request.registry['res.users'].has_group(request.cr, request.uid, 'base.group_website_publisher')
-            if partner.exists() and (partner.website_published or is_website_publisher):
-                values = {
-                    'main_object': partner,
-                    'partner': partner,
-                    'current_grade': current_grade,
-                    'current_country': current_country
-                }
-                return request.website.render("website_crm_partner_assign.partner", values)
+        partner_sudo = request.env['res.partner'].sudo().browse(partner_id)
+        is_website_publisher = request.env['res.users'].has_group('base.group_website_publisher')
+        if partner_sudo.exists() and (partner_sudo.website_published or is_website_publisher):
+            values = dict(main_object=partner_sudo, partner=partner_sudo, current_grade=None, current_country=None)
+            if grade_id:
+                values['current_grade'] = request.env['res.partner.grade'].browse(int(grade_id)).exists()
+            if country_id:
+                values['current_country'] = request.env['res.country'].browse(int(country_id)).exists()
+            return request.website.render("website_crm_partner_assign.partner", values)
         return self.partners(**post)

lukebranch/odoo

addons/website_crm_partner_assign/controllers/main.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
+
 import werkzeug
-from openerp import SUPERUSER_ID
-from openerp.addons.web import http
-from openerp.addons.web.http import request
+from openerp import http, _
+from openerp.http import request
 from openerp.addons.website.models.website import slug, unslug
 from openerp.addons.website_partner.controllers.main import WebsitePartnerPage
-from openerp.tools.translate import _
 
 
 class WebsiteCrmPartnerAssign(WebsitePartnerPage):
@@ -26,12 +25,12 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
     ], type='http', auth="public", website=True)
     def partners(self, country=None, grade=None, page=0, **post):
         country_all = post.pop('country_all', False)
-        partner_obj = request.registry['res.partner']
-        country_obj = request.registry['res.country']
+        PartnerSudo = request.env['res.partner'].sudo()
         search = post.get('search', '')
+        grade_count, country_count = 0, 0
 
         base_partner_domain = [('is_company', '=', True), ('grade_id', '!=', False), ('website_published', '=', True)]
-        if not request.registry['res.users'].has_group(request.cr, request.uid, 'base.group_website_publisher'):
+        if not request.env['res.users'].has_group('base.group_website_publisher'):
             base_partner_domain += [('grade_id.website_published', '=', True)]
         if search:
             base_partner_domain += ['|', ('name', 'ilike', search), ('website_description', 'ilike', search)]
@@ -41,22 +40,16 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
         if not country and not country_all:
             country_code = request.session['geoip'].get('country_code')
             if country_code:
-                country_ids = country_obj.search(request.cr, request.uid, [('code', '=', country_code)], context=request.context)
-                if country_ids:
-                    country = country_obj.browse(request.cr, request.uid, country_ids[0], context=request.context)
+                country = request.env['res.country'].search([('code', '=', country_code)], limit=1)
         if country:
             grade_domain += [('country_id', '=', country.id)]
-        grades = partner_obj.read_group(
-            request.cr, SUPERUSER_ID, grade_domain, ["id", "grade_id"],
-            groupby="grade_id", orderby="grade_id DESC", context=request.context)
-        grades_partners = partner_obj.search(
-            request.cr, SUPERUSER_ID, grade_domain,
-            context=request.context, count=True)
+        grades = PartnerSudo.read_group(grade_domain, ["id", "grade_id"], groupby="grade_id", orderby="grade_id DESC")
         # flag active grade
         for grade_dict in grades:
             grade_dict['active'] = grade and grade_dict['grade_id'][0] == grade.id
+            grade_count += grade_dict['grade_id_count']
         grades.insert(0, {
-            'grade_id_count': grades_partners,
+            'grade_id_count': grade_count,
             'grade_id': (0, _("All Categories")),
             'active': bool(grade is None),
         })
@@ -65,17 +58,13 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
         country_domain = list(base_partner_domain)
         if grade:
             country_domain += [('grade_id', '=', grade.id)]
-        countries = partner_obj.read_group(
-            request.cr, SUPERUSER_ID, country_domain, ["id", "country_id"],
-            groupby="country_id", orderby="country_id", context=request.context)
-        countries_partners = partner_obj.search(
-            request.cr, SUPERUSER_ID, country_domain,
-            context=request.context, count=True)
+        countries = PartnerSudo.read_group(country_domain, ["id", "country_id"], groupby="country_id", orderby="country_id")
         # flag active country
         for country_dict in countries:
             country_dict['active'] = country and country_dict['country_id'] and country_dict['country_id'][0] == country.id
+            country_count += country_dict['country_id_count']
         countries.insert(0, {
-            'country_id_count': countries_partners,
+            'country_id_count': country_count,
             'country_id': (0, _("All Countries")),
             'active': bool(country is None),
         })
@@ -101,19 +90,13 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
         if country_all:
             url_args['country_all'] = True
 
-        partner_count = partner_obj.search_count(
-            request.cr, SUPERUSER_ID, base_partner_domain,
-            context=request.context)
+        partner_count = PartnerSudo.search_count(base_partner_domain)
         pager = request.website.pager(
             url=url, total=partner_count, page=page, step=self._references_per_page, scope=7,
             url_args=url_args)
 
         # search partners matching current search parameters
-        partner_ids = partner_obj.search(
-            request.cr, SUPERUSER_ID, base_partner_domain,
-            order="grade_id DESC",
-            context=request.context)  # todo in trunk: order="grade_id DESC, implemented_count DESC", offset=pager['offset'], limit=self._references_per_page
-        partners = partner_obj.browse(request.cr, SUPERUSER_ID, partner_ids, request.context)
+        partners = PartnerSudo.search(base_partner_domain, order="grade_id DESC")  # todo in trunk: order="grade_id DESC, implemented_count DESC", offset=pager['offset'], limit=self._references_per_page
         # remove me in trunk
         partners = sorted(partners, key=lambda x: (x.grade_id.sequence if x.grade_id else 0, len([i for i in x.implemented_partner_ids if i.website_published])), reverse=True)
         partners = partners[pager['offset']:pager['offset'] + self._references_per_page]
@@ -137,26 +120,15 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage):
     @http.route(['/partners/<partner_id>'], type='http', auth="public", website=True)
     def partners_detail(self, partner_id, **post):
         _, partner_id = unslug(partner_id)
-        current_grade, current_country = None, None
         grade_id = post.get('grade_id')
         country_id = post.get('country_id')
-        if grade_id:
-            grade_ids = request.registry['res.partner.grade'].exists(request.cr, request.uid, int(grade_id), context=request.context)
-            if grade_ids:
-                current_grade = request.registry['res.partner.grade'].browse(request.cr, request.uid, grade_ids[0], context=request.context)
-        if country_id:
-            country_ids = request.registry['res.country'].exists(request.cr, request.uid, int(country_id), context=request.context)
-            if country_ids:
-                current_country = request.registry['res.country'].browse(request.cr, request.uid, country_ids[0], context=request.context)
-        if partner_id:
-            partner = request.registry['res.partner'].browse(request.cr, SUPERUSER_ID, partner_id, context=request.context)
-            is_website_publisher = request.registry['res.users'].has_group(request.cr, request.uid, 'base.group_website_publisher')
-            if partner.exists() and (partner.website_published or is_website_publisher):
-                values = {
-                    'main_object': partner,
-                    'partner': partner,
-                    'current_grade': current_grade,
-                    'current_country': current_country
-                }
-                return request.website.render("website_crm_partner_assign.partner", values)
+        partner_sudo = request.env['res.partner'].sudo().browse(partner_id)
+        is_website_publisher = request.env['res.users'].has_group('base.group_website_publisher')
+        if partner_sudo.exists() and (partner_sudo.website_published or is_website_publisher):
+            values = dict(main_object=partner_sudo, partner=partner_sudo, current_grade=None, current_country=None)
+            if grade_id:
+                values['current_grade'] = request.env['res.partner.grade'].browse(int(grade_id)).exists()
+            if country_id:
+                values['current_country'] = request.env['res.country'].browse(int(country_id)).exists()
+            return request.website.render("website_crm_partner_assign.partner", values)
         return self.partners(**post)